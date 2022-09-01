d05563991e4e7f62634dae83e3a57d5e95cb176a
Vitor Honna <vitorhonna@gmail.com>
2022-09-01 00:57:29 -0300

    first tests

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,19 +1,33 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/my-first-api')
-def hello():
-    return {"message": "Hello world!"}
+class User(BaseModel):
+    name: str
+    email: str
+    user_id: str
 
 
-@app.get("/hello")
-def hello_user(name: str | None = None):
-    if name is None:
-        text = 'Hello!'
-    else:
-        text = f'Hello, {name}!'
-    return {"message": text}
+@app.get("/users/all")
+def all_users():
+    return {"message": 'all users'}
 
 
+@app.get("/users/{user_id}")
+def get_user(user_id: str | None = None):
+    if not user_id:
+        return {"message": 'Please provide an user ID: /users?id=USER_ID'}
+
+    return {'user': f'specific user, id = {user_id}'}
+
+
+@app.post("/users/add")
+def add_user(user: User):
+    pass
+
+
+@app.put("/users/{user_id}")
+def update_user(user_id: str, user: User):
+    return {"user_name": user.name, "user_id": user_id}
--- fast-api/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-print()
-
-print(requests.get('http://localhost:8000/my-first-api').text)
-print()
-print(requests.get('http://localhost:8000/hello').text)
-print(requests.get('http://localhost:8000/hello?name=Vitor').text)
